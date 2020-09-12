2d8651c342fb95b5802d51a21730529bd562e765
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-09-12 11:07:59 +0100

    adding checkout page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Home from './Home.js';
+import Checkout from './Checkout.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM convention - changing classname from App to app
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    // Wrap the app in the Router
+    <Router>
+      <div className="app">
+        {/* As the header will always be rendered regardless of the route, we put it outside of the switch statement */}
+        <Header />
+        {/* Adding the switch will allow us to create routes */}
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          {/* Route path will then render the selected components when we are on the home page - default page (/) */}
+          {/* The Default route will always need to be at the bottom of the routes created */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
