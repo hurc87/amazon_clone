751c6c5de0f01eafeb23dce101c23330bc8decaf
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-09-12 17:44:55 +0100

    added shopping cart, along with remove from shopping cart button

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer, { initialState } from './reducer';
+import { StateProvider } from './StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Wraps the app in the state provider */}
+    {/* initial state - what does the data layer look like in the beginning */}
+    {/* reducer - how can we manipulate / play with the data layer */}
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
